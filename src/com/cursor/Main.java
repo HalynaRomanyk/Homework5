9659d71265645fa1587f1d52b4228adc3b0e11b3
Haliya Romanyk <haliya.romanyk@gmail.com>
2022-01-03 19:00:30 +0200

    Homework5

---
+++ src/com/cursor/Main.java
@@ -0,0 +1,33 @@
+package com.cursor;
+
+public class Main {
+
+    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+        boolean result = validateCredentials("123", "Abc", "Abc");
+        System.out.println(result);
+
+    }
+
+    public static boolean validateCredentials(String login, String password, String confirmPasword) throws WrongLoginException,
+            WrongPasswordException {
+        try {
+            if (!login.matches("^[a-zA-Z0-9_]+$") || login.length() >= 20) {
+                throw new WrongLoginException("Wrong login");
+            }
+            if (!password.matches("^[a-zA-Z0-9_]+$") || password.length() >= 20 || !password.equals(confirmPasword)) {
+                throw new WrongPasswordException("Wrong password");
+            }
+        } catch (WrongLoginException e) {
+            e.printStackTrace();
+            return false;
+        } catch (WrongPasswordException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
+
+//        Обработка исключений проводится внутри метода.
+//        Используем multi-catch block. Метод возвращает true,
+//        если значения верны или false в другом случае.
---
+++ src/com/cursor/WrongLoginException.java
@@ -0,0 +1,10 @@
+package com.cursor;
+
+public class WrongLoginException extends Exception{
+    public WrongLoginException() {
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
