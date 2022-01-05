221577cdfdb1d698c295f78c5e0373824eda8f64
HalynaRomanyk <96209010+HalynaRomanyk@users.noreply.github.com>
2022-01-05 21:15:31 +0200

    Homework5 formatted code

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -1,15 +1,12 @@
 package com.cursor;
 
 public class Main {
-
     public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
         boolean result = validateCredentials("123", "Abc", "Abc");
         System.out.println(result);
-
     }
 
-    public static boolean validateCredentials(String login, String password, String confirmPasword) throws WrongLoginException,
-            WrongPasswordException {
+    public static boolean validateCredentials(String login, String password, String confirmPasword) throws WrongLoginException, WrongPasswordException {
         try {
             if (!login.matches("^[a-zA-Z0-9_]+$") || login.length() >= 20) {
                 throw new WrongLoginException("Wrong login");
--- src/com/cursor/WrongLoginException.java
+++ src/com/cursor/WrongLoginException.java
@@ -1,6 +1,6 @@
 package com.cursor;
 
-public class WrongLoginException extends Exception{
+public class WrongLoginException extends Exception {
     public WrongLoginException() {
     }
 
